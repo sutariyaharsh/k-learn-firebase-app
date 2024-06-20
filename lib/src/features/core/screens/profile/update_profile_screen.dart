e341d4bdec6d3b2a4b9f3c793fb0ac9590831226
krupaliL <lkrupali312@gmail.com>
2024-06-20 18:07:18 +0530

    Retrieve all users

--- lib/src/features/core/screens/profile/update_profile_screen.dart
+++ lib/src/features/core/screens/profile/update_profile_screen.dart
@@ -24,105 +24,35 @@ class UpdateProfileScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(tDefaultSize),
-          child: FutureBuilder(
-            future: controller.getUserData(),
+          child: FutureBuilder<List<UserModel>>(
+            future: controller.getAllUser(),
             builder: (context, snapshot){
               if(snapshot.connectionState == ConnectionState.done){
                 if(snapshot.hasData){
-                  UserModel userData = snapshot.data as UserModel;
-                  return Column( /// Step 4 - Wrap this Widget with FutureBuilder
-                    children: [
-                      // -- IMAGE with ICON
-                      Stack(
-                        children: [
-                          SizedBox(
-                            width: 120,
-                            height: 120,
-                            child: ClipRRect(
-                                borderRadius: BorderRadius.circular(100), child: const Image(image: AssetImage(tProfileImage))),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            right: 0,
-                            child: Container(
-                              width: 35,
-                              height: 35,
-                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: tPrimaryColor),
-                              child: const Icon(
-                                  LineAwesomeIcons.camera_solid,
-                                  color: Colors.black,
-                                  size: 20
+                  // UserModel userData = snapshot.data as UserModel;
+                  return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (c, index) {
+                        return Column(
+                          children: [
+                            ListTile(
+                              iconColor: Colors.blue,
+                              tileColor: Colors.blue.withOpacity(0.1),
+                              leading: const Icon(LineAwesomeIcons.user),
+                              title: Text("Name: ${snapshot.data![index].fullName}"),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(snapshot.data![index].phoneNo),
+                                  Text(snapshot.data![index].email),
+                                ],
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 50),
-
-                      // -- Form Fields
-                      Form(child: Column(
-                        children: [
-                          TextFormField(
-                            initialValue: userData.fullName,
-                            decoration: const InputDecoration(label: Text(tFullName), prefixIcon: Icon(LineAwesomeIcons.user),),
-                          ),
-                          const SizedBox(height: tFormHeight - 20,),
-                          TextFormField(
-                            initialValue: userData.email,
-                            decoration: const InputDecoration(label: Text(tEmail), prefixIcon: Icon(LineAwesomeIcons.envelope_solid),),
-                          ),
-                          const SizedBox(height: tFormHeight - 20,),
-                          TextFormField(
-                            initialValue: userData.phoneNo,
-                            decoration: const InputDecoration(label: Text(tPhoneNo), prefixIcon: Icon(LineAwesomeIcons.phone_solid),),
-                          ),
-                          const SizedBox(height: tFormHeight - 20,),
-                          TextFormField(
-                            initialValue: userData.password,
-                            decoration: const InputDecoration(label: Text(tPassword), prefixIcon: Icon(LineAwesomeIcons.fingerprint_solid),),
-                          ),
-                          const SizedBox(height: tFormHeight),
-                          SizedBox(
-                            width: double.infinity,
-                            child: ElevatedButton(
-                              onPressed: () => Get.to(() => const UpdateProfileScreen()),
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: tPrimaryColor,
-                                  side: BorderSide.none,
-                                  shape: const StadiumBorder()),
-                              child: const Text(tEditProfile,
-                                  style: TextStyle(color: tDarkColor)),
-                            ),
-                          ),
-                          const SizedBox(height: tFormHeight),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Text.rich(
-                                TextSpan(
-                                  text: tJoined,
-                                  style: TextStyle(fontSize: 12),
-                                  children: [
-                                    TextSpan(text: tJoinedAt, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12))
-                                  ],
-                                ),
-                              ),
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent.withOpacity(0.1),
-                                  elevation: 0,
-                                  foregroundColor: Colors.red,
-                                  shape: const StadiumBorder(),
-                                  side: BorderSide.none,
-                                ),
-                                child: const Text(tDelete),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                      ),
-                    ],
+                            const SizedBox(height: 10,)
+                          ],
+                        );
+                      }
                   );
                 } else if(snapshot.hasError){
                   return Center(child: Text(snapshot.error.toString()));
