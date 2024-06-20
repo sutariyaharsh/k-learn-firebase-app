1a018fae8c09ea0a388b4eb17d20eee8550a88f1
krupaliL <lkrupali312@gmail.com>
2024-06-20 18:00:48 +0530

    Solve runtime error of update_profile_screen

--- lib/src/features/core/screens/profile/update_profile_screen.dart
+++ lib/src/features/core/screens/profile/update_profile_screen.dart
@@ -25,110 +25,104 @@ class UpdateProfileScreen extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.all(tDefaultSize),
           child: FutureBuilder(
-            future: controller.getAllUser(),
+            future: controller.getUserData(),
             builder: (context, snapshot){
               if(snapshot.connectionState == ConnectionState.done){
                 if(snapshot.hasData){
                   UserModel userData = snapshot.data as UserModel;
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (c, index) {
-                      return Column( /// Step 4 - Wrap this Widget with FutureBuilder
+                  return Column( /// Step 4 - Wrap this Widget with FutureBuilder
+                    children: [
+                      // -- IMAGE with ICON
+                      Stack(
                         children: [
-                          // -- IMAGE with ICON
-                          Stack(
-                            children: [
-                              SizedBox(
-                                width: 120,
-                                height: 120,
-                                child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(100), child: const Image(image: AssetImage(tProfileImage))),
-                              ),
-                              Positioned(
-                                bottom: 0,
-                                right: 0,
-                                child: Container(
-                                  width: 35,
-                                  height: 35,
-                                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: tPrimaryColor),
-                                  child: const Icon(
-                                      LineAwesomeIcons.camera_solid,
-                                      color: Colors.black,
-                                      size: 20
-                                  ),
-                                ),
-                              ),
-                            ],
+                          SizedBox(
+                            width: 120,
+                            height: 120,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(100), child: const Image(image: AssetImage(tProfileImage))),
                           ),
-                          const SizedBox(height: 50),
-
-                          // -- Form Fields
-                          Form(child: Column(
-                            children: [
-                              TextFormField(
-                                initialValue: userData.fullName,
-                                decoration: const InputDecoration(label: Text(tFullName), prefixIcon: Icon(LineAwesomeIcons.user),),
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: Container(
+                              width: 35,
+                              height: 35,
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: tPrimaryColor),
+                              child: const Icon(
+                                  LineAwesomeIcons.camera_solid,
+                                  color: Colors.black,
+                                  size: 20
                               ),
-                              const SizedBox(height: tFormHeight - 20,),
-                              TextFormField(
-                                initialValue: userData.email,
-                                decoration: const InputDecoration(label: Text(tEmail), prefixIcon: Icon(LineAwesomeIcons.envelope_solid),),
-                              ),
-                              const SizedBox(height: tFormHeight - 20,),
-                              TextFormField(
-                                initialValue: userData.phoneNo,
-                                decoration: const InputDecoration(label: Text(tPhoneNo), prefixIcon: Icon(LineAwesomeIcons.phone_solid),),
-                              ),
-                              const SizedBox(height: tFormHeight - 20,),
-                              TextFormField(
-                                initialValue: userData.password,
-                                decoration: const InputDecoration(label: Text(tPassword), prefixIcon: Icon(LineAwesomeIcons.fingerprint_solid),),
-                              ),
-                              const SizedBox(height: tFormHeight),
-                              SizedBox(
-                                width: double.infinity,
-                                child: ElevatedButton(
-                                  onPressed: () => Get.to(() => const UpdateProfileScreen()),
-                                  style: ElevatedButton.styleFrom(
-                                      backgroundColor: tPrimaryColor,
-                                      side: BorderSide.none,
-                                      shape: const StadiumBorder()),
-                                  child: const Text(tEditProfile,
-                                      style: TextStyle(color: tDarkColor)),
-                                ),
-                              ),
-                              const SizedBox(height: tFormHeight),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Text.rich(
-                                    TextSpan(
-                                      text: tJoined,
-                                      style: TextStyle(fontSize: 12),
-                                      children: [
-                                        TextSpan(text: tJoinedAt, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12))
-                                      ],
-                                    ),
-                                  ),
-                                  ElevatedButton(
-                                    onPressed: () {},
-                                    style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent.withOpacity(0.1),
-                                      elevation: 0,
-                                      foregroundColor: Colors.red,
-                                      shape: const StadiumBorder(),
-                                      side: BorderSide.none,
-                                    ),
-                                    child: const Text(tDelete),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
+                            ),
                           ),
                         ],
-                      );
-                    }
+                      ),
+                      const SizedBox(height: 50),
+
+                      // -- Form Fields
+                      Form(child: Column(
+                        children: [
+                          TextFormField(
+                            initialValue: userData.fullName,
+                            decoration: const InputDecoration(label: Text(tFullName), prefixIcon: Icon(LineAwesomeIcons.user),),
+                          ),
+                          const SizedBox(height: tFormHeight - 20,),
+                          TextFormField(
+                            initialValue: userData.email,
+                            decoration: const InputDecoration(label: Text(tEmail), prefixIcon: Icon(LineAwesomeIcons.envelope_solid),),
+                          ),
+                          const SizedBox(height: tFormHeight - 20,),
+                          TextFormField(
+                            initialValue: userData.phoneNo,
+                            decoration: const InputDecoration(label: Text(tPhoneNo), prefixIcon: Icon(LineAwesomeIcons.phone_solid),),
+                          ),
+                          const SizedBox(height: tFormHeight - 20,),
+                          TextFormField(
+                            initialValue: userData.password,
+                            decoration: const InputDecoration(label: Text(tPassword), prefixIcon: Icon(LineAwesomeIcons.fingerprint_solid),),
+                          ),
+                          const SizedBox(height: tFormHeight),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () => Get.to(() => const UpdateProfileScreen()),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: tPrimaryColor,
+                                  side: BorderSide.none,
+                                  shape: const StadiumBorder()),
+                              child: const Text(tEditProfile,
+                                  style: TextStyle(color: tDarkColor)),
+                            ),
+                          ),
+                          const SizedBox(height: tFormHeight),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text.rich(
+                                TextSpan(
+                                  text: tJoined,
+                                  style: TextStyle(fontSize: 12),
+                                  children: [
+                                    TextSpan(text: tJoinedAt, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12))
+                                  ],
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent.withOpacity(0.1),
+                                  elevation: 0,
+                                  foregroundColor: Colors.red,
+                                  shape: const StadiumBorder(),
+                                  side: BorderSide.none,
+                                ),
+                                child: const Text(tDelete),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      ),
+                    ],
                   );
                 } else if(snapshot.hasError){
                   return Center(child: Text(snapshot.error.toString()));
